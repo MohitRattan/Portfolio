776a59e52a09bfcbcdbf10f6a6ddaa53e59c5736
MohitRattan <mohitrattan090996@gmail.com>
2025-01-17 21:56:28 +0530

    about

--- src/app/Components/about/about.component.ts
+++ src/app/Components/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    const elements = document.querySelectorAll('.animate-fade-in, .animate-slide-in');
+    elements.forEach((el) => {
+      const rect = el.getBoundingClientRect();
+      if (rect.top < window.innerHeight) {
+        el.classList.add('visible');
+      }
+    });
+  }
 }
